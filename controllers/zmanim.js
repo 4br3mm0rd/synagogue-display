59b4c4e7296f7ac2c8b84e86e60b57c3930427c4
Emmanuel Ouanounou <ouanounou@supelec.fr>
2020-02-15 22:30:47 +0200

    added some events info

--- controllers/zmanim.js
+++ controllers/zmanim.js
@@ -1,6 +1,33 @@
 const { ComplexZmanimCalendar, getZmanimJson } = require("kosher-zmanim");
 const Hebcal = require('hebcal');
 const months = require('../helpers/jewish-months');
+const days = require('../helpers/days');
+
+function hdatecmp(hdate1, hdate2){
+    return hdate1.day == hdate2.day && hdate1.month == hdate2.month && hdate1.year == hdate2.year;
+}
+
+function isRoshHodesh(hdate){
+    var next_month = hdate.month + 1;
+    if(next_month == 13 && !hdate.isLeapYear()) next_month = 1;
+    if(next_month == 14) next_month = 1;
+
+    var this_rosh_hodesh = (new Hebcal.Month(hdate.month, hdate.year)).rosh_chodesh();
+    var next_rosh_hodesh = (new Hebcal.Month(next_month, hdate.year)).rosh_chodesh();
+
+    if(hdatecmp(hdate, this_rosh_hodesh)) return true;
+    if(hdatecmp(hdate, next_rosh_hodesh)) return true;
+
+    return false;
+}
+
+function getEvent(hdate){
+    var holidays_this_month = new Hebcal.Month(hdate.month, hdate.year).holidays;
+    for(const event in holidays_this_month){
+        if(hdatecmp(hdate, holidays_this_month[event][0].date)) return holidays_this_month[event][0].desc;
+    }
+    return {};
+}
 
 module.exports = {
     zmanim: function(date){
@@ -20,23 +47,28 @@ module.exports = {
             minha: zmanim.MinchaGedola,
             candle: zmanim.CandleLighting,
             shkia: zmanim.Sunset,
-            tzet: zmanim.Tzais
+            tzet: zmanim.Tzais,
+            havdallah: zmanim.Havdallah
         };
+        //TODO: compute real zman of havdallah and candle light
         return result;
     },
 
     date: function(date){
-        var hdate = Hebcal.HDate(new Date("April 8, 2020"));
-        hdate.setCity('Ramat Gan');
-        hdate.setLocation(32, 35);
+        var hdate = Hebcal.HDate(date);
+
         return {
             date: date,
             dayOfWeek: hdate.getDay(),
             day: hdate.day,
+            dayFR: days.french[hdate.getDay() - 1],
+            dayHE: days.hebrew[hdate.getDay() - 1],
             monthFR: months.french[hdate.month - 1],
             monthHE: months.hebrew[months.french[hdate.month - 1]],
             year: hdate.year,
-            parasha: hdate.getSedra('h')
+            parasha: hdate.getSedra('h'),
+            rosh_hodesh: isRoshHodesh(hdate),
+            getEvent: getEvent(hdate)
         };
     }
 };
---
+++ helpers/days.js
@@ -0,0 +1,20 @@
+module.exports = {
+    french: [
+        "Lundi",
+        "Mardi",
+        "Mercredi",
+        "Jeudi",
+        "Vendredi",
+        "Samedi",
+        "Dimanche"
+    ],
+    hebrew: [
+        "יום ב",
+        "יום ג",
+        "יום ד",
+        "יום ה",
+        "יום ו",
+        "יום ז",
+        "יום א"
+    ]
+}
